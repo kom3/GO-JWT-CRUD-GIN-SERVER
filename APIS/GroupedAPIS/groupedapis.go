a06a7cdc68d2501eceaf0187b98a7c49dfc297f6
kom3 <manjunathkankatteko@gmail.com>
2022-07-31 22:50:05 +0530

    added dummy routes group

--- APIS/GroupedAPIS/groupedapis.go
+++ APIS/GroupedAPIS/groupedapis.go
@@ -1,1 +1,31 @@
 package GroupedAPIS
+
+// routes.go
+// func initializeRoutes() {
+
+// 	router.Use(setUserStatus())
+
+// 	router.GET("/", showIndexPage)
+
+// 	userRoutes := router.Group("/u")
+// 	{
+// 		userRoutes.GET("/login", ensureNotLoggedIn(), showLoginPage)
+
+// 		userRoutes.POST("/login", ensureNotLoggedIn(), performLogin)
+
+// 		userRoutes.GET("/logout", ensureLoggedIn(), logout)
+
+// 		userRoutes.GET("/register", ensureNotLoggedIn(), showRegistrationPage)
+
+// 		userRoutes.POST("/register", ensureNotLoggedIn(), register)
+// 	}
+
+// 	articleRoutes := router.Group("/article")
+// 	{
+// 		articleRoutes.GET("/view/:article_id", getArticle)
+
+// 		articleRoutes.GET("/create", ensureLoggedIn(), showArticleCreationPage)
+
+// 		articleRoutes.POST("/create", ensureLoggedIn(), createArticle)
+// 	}
+// }
